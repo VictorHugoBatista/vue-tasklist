23cddb044a66ff4a32f070f649a73084b510da17
Victor Hugo Batista <victor.batista@i9xp.com.br>
2020-02-07 10:46:32 -0300

    Style form with bootstrap

--- src/components/todo/updater-form.vue
+++ src/components/todo/updater-form.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <form v-on:submit.once="addItemToList">
-      <input type="text" ref="field-text" />
-      <button v-on:click="addItemToList">Add</button>
-    </form>
-    <button v-if="0 < this.$store.state.todo.listItems.length"
-      v-on:click="clearList">Clear list</button>
+    <b-form inline v-on:submit.once="addItemToList" class="my-3">
+      <b-form-group>
+        <b-form-input type="text" ref="field-text" class="mr-3"></b-form-input>
+        <b-button variant="primary" v-on:click="addItemToList" class="mr-3">Add</b-button>
+        <b-button variant="primary" v-if="0 < this.$store.state.todo.listItems.length"
+          v-on:click="clearList">Clear list</b-button>
+      </b-form-group>
+    </b-form>
   </div>
 </template>
 
